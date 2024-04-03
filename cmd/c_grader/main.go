776c2b0df66864cbaf3a1d5d353faa08ee8fa141
VinofSteel <v.nascimento23@yahoo.com.br>
2024-04-03 01:15:06 -0300

    chore: adding Fiber framework and initial configs for it

--- cmd/c_grader/main.go
+++ cmd/c_grader/main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/VinOfSteel/cinemagrader/initializers"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	// Initializing environment variables
 	initializers.InitializeEnv()
+
+	// Starting fiber
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen(fmt.Sprintf(":%v", os.Getenv("PORT")))
 }
